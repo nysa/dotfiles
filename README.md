a0ff42fd5b4e8fa40a99ae1ea0b575b07c9f0667
Nysa <nysa@nysavann.com>
2015-02-15 22:08:47 -0600

    Update README.md

--- README.md
+++ README.md
@@ -6,7 +6,5 @@ Install using `rake`.
 
 ```
 $ git clone https://github.com/nysa/dotfiles.git ~/dotfiles
-$ cd ~/dotfiles
-$ rake
+$ ~/dotfiles/install.sh
 ```
-
--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ echo "Symlinking dotfiles..."
 for f in ack/* bin/* git/* vim/* ruby/* sbt/*; do
   filepath=( ${f//\// } )
   filename=${filepath[$((${#filepath[@]} - 1))]}
-  src=$(pwd)/$f
-  target=$HOME/.$filename
+  src="$(pwd)/$f"
+  target="$HOME/.$filename"
 
   if [ -h "$target" ]; then
     unlink $target
