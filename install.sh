61100709a347f7f2e41990321525d212715613eb
Nysa <nysa@nysavann.com>
2015-02-15 22:05:24 -0600

    Port Rakefile to shell script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd "$( dirname "$0" )"
+
+echo "Symlinking dotfiles..."
+for f in ack/* bin/* git/* vim/* ruby/* sbt/*; do
+  filepath=( ${f//\// } )
+  filename=${filepath[$((${#filepath[@]} - 1))]}
+  src=$(pwd)/$f
+  target=$HOME/.$filename
+
+  if [ -h "$target" ]; then
+    unlink $target
+  fi
+
+  ln -vsf $src $target
+done
+echo "Dotfiles symlinked!"
+
+echo "Installing submodules..."
+git submodule update --init
+echo "Submodules installed!"
+
+echo "Installing Vundle bundles..."
+vim +BundleInstall +qall
+echo "Vundle bundles installed!"
